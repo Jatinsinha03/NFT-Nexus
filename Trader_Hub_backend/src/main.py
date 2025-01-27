36c9b58d32933e31b5e86e10ce52c5d7676bceb7
Jatin Sinha <jatinsinha03@gmail.com>
2025-01-28 00:43:36 +0530

    Corrected port for deployement

--- Trader_Hub_backend/src/main.py
+++ Trader_Hub_backend/src/main.py
@@ -49,4 +49,4 @@ async def stream_rag_output(request: Request):
     return res
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", port=int(port), reload=True, log_level="info")   
+    uvicorn.run("main:app", host="0.0.0.0", port=int(port), reload=True, log_level="info")
